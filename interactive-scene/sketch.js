d1811bf39d52c8291c68ac2d6daa30482b530a3e
MoizNQ <moizsims9@gmail.com>
2022-09-21 12:01:12 -0600

    interactive-scene

---
+++ interactive-scene/sketch.js
@@ -0,0 +1,17 @@
+// Shooting Practise
+// Moiz Naqvi
+// 9/21/2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("black");
+  fill("white");
+  circle(mouseX, mouseY, 5);
+}
