91c0e51b77175c8e7cf74085a533e408bda8ed62
MoizNQ <moizsims9@gmail.com>
2022-09-29 10:39:38 -0600

    crosshair

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,17 +1,44 @@
 // Shooting Practise
 // Moiz Naqvi
-// 9/21/2022
+// Sept 21, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let mapp;
+let crosshair;
+let figuree;
+let scalar = 0.3;
+let button = false;
+let theTime;
+let enemyKilled;
+let mouseCursor;
+
+function preload() {
+  mapp = loadImage("Map.jpeg");
+  crosshair = loadImage("crosshair.jpeg");
+  figuree = loadImage("figure.jpeg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // hiding cursor
+  noCursor();
 }
 
 function draw() {
-  background("black");
-  fill("white");
-  circle(mouseX, mouseY, 5);
+  background(220);
+  image(mapp, 0, 0, windowWidth, windowHeight);
+  image(figuree, 0, 0, 80, 80);
+  image(crosshair, mouseX, mouseY, crosshair.width*scalar, crosshair.height*scalar);
+  keyTyped();
+}
+
+function keyTyped() {
+  if (key === UP_ARROW) {
+    scalar = + 0.01;
+  }
+  else if (key === DOWN_ARROW) {
+    scalar = - 0.01;
+  }
 }
