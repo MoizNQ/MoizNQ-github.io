299dbfd56ab3d17da982688e7486aaf76bf6cab6
MoizNQ <moizsims9@gmail.com>
2022-10-03 12:16:16 -0600

    main and start

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -13,6 +13,7 @@ let button = false;
 let theTime;
 let enemyKilled;
 let mouseCursor;
+let state = "start";
 
 function preload() {
   mapp = loadImage("Map.jpeg");
@@ -23,15 +24,47 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // hiding cursor
-  noCursor();
+  if (state === "main") {
+    noCursor();
+  }
 }
 
 function draw() {
   background(220);
   image(mapp, 0, 0, windowWidth, windowHeight);
-  image(figuree, 0, 0, 80, 80);
-  image(crosshair, mouseX, mouseY, crosshair.width*scalar, crosshair.height*scalar);
+
   keyIsDown();
+  if (state === "start") {
+    startScreen();
+  }
+  if (state === "main") {
+    image(mapp, 0, 0, windowWidth, windowHeight);
+    image(figuree, 0, 0, 80, 80);
+    image(crosshair, mouseX, mouseY, crosshair.width*scalar, crosshair.height*scalar);
+  }
+}
+
+function mousePressed() {
+  if (state === "start" && mouseInsideRect(450, 700,450, 550)) {
+    state = "main";
+  }
+}
+
+function startScreen() {
+  if (mouseInsideRect(width/2.5, height/2.5, 250, 150)) {
+    fill("gray");
+  }
+  else {
+    fill("black");
+  }
+  rect(width/2.5, height/2.5, 250, 150);
+  fill("white");
+  textSize(50);
+  text("START", 425, 425);
+}
+
+function mouseInsideRect(left, right, top, bottom) {
+  return mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom;
 }
 
 // eslint-disable-next-line no-redeclare
