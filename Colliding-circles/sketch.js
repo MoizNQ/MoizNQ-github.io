0943745faabbb3027c5e3fef9c2ecb5687730792
MoizNQ <moizsims9@gmail.com>
2022-10-25 12:17:17 -0600

    playful

--- Colliding-circles/sketch.js
+++ Colliding-circles/sketch.js
@@ -19,10 +19,26 @@ function draw() {
     theCircles[i].x += theCircles[i].dx;
     theCircles[i].y += theCircles[i].dy;
 
+    // collision check
+    for (let j = 0; j < theCircles.length; j++) {
+      if( i !== j) { //don't check if hitting itself
+        if(isColliding(theCircles[i], theCircles[j])) {
+          let tempDx = theCircles[i].dx;
+          let tempDy = theCircles[i].dy;
+          theCircles[i].dx = theCircles[j].dx;
+          theCircles[i].dy = theCircles[j].dy;
+          theCircles[js].dx = tempDx;
+          theCircles[j].dy = tempDy;
+          
+        }
+      }
+    }
+
+    // left-rigjt edges
     if(theCircles[i]. x + theCircles[i].radius > width || theCircles[i].x - theCircles[i].radius < 0) {
       theCircles[i].dx *= -1;
     }
-    
+    // top-bottom edges
     if(theCircles[i]. y + theCircles[i].radius > height || theCircles[i].y - theCircles[i].radius < 0) {
       theCircles[i].dy *= -1;
 
@@ -33,6 +49,17 @@ function draw() {
       noStroke();
       circle(thisCircle.x, thisCircle.y, thisCircle.radius*2);
     }
+  }
+}
+
+function isColliding(ball1, ball2) {
+  let distanceBetween = dist(ball1.x, ball1.y, ball2.x, ball2.y);
+  let radiiSum = ball1.radius + ball2.radius;
+  if (distanceBetween > radiiSum) {
+    return false;
+  }
+  else {
+    return true;
   }
 }
 
@@ -47,7 +74,7 @@ function spawnBall(tempX, tempY) {
     radius: random(25, 100),
     dx: random(-5, 5),
     dy: random(-5, 5),
-    theColor: color(random(255), random(255), random(255), random(255),)
+    theColor: color(random(255), random(255), random(255), random(255))
   };
   return newBall;
 
