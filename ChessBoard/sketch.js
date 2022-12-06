136b19c0b713426524a30126e79063d382722ea1
MoizNQ <moizsims9@gmail.com>
2022-12-06 11:30:55 -0600

    Function and states
    Putting in function and states for different options that will be displayed when the game begins

--- ChessBoard/sketch.js
+++ ChessBoard/sketch.js
@@ -1,15 +1,35 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Local Stoage Demo
 
+let numberOfClicks = 0;
+let highestEver = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  if(getItem("highscore") !== null) {
+    highestEver = getItem("highscore");
+  }
+  else {
+    storeItem("highscore" , 0);
+  }
 }
 
 function draw() {
-  background("black");
+  background(255);
+
+  fill(0);
+  textSize(50);
+  text(numberOfClicks, width/2, height/2);
+
+  fill("red");
+  text(highestEver, 50, height - 100);
+}
+
+function mousePressed() {
+  numberOfClicks++;
+
+  if (numberOfClicks > getItem("highscore")) {
+    storeItem("highscore", numberOfClicks);
+    highestEver = numberOfClicks;
+  }
 }
